318ec68ef427faaffece82248939787f4fed1a3b
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2022-04-02 20:25:24 +0700

    :wrench: Back to axios

--- src/actions/sellerAction.js
+++ src/actions/sellerAction.js
@@ -2,50 +2,50 @@ import Axios from 'axios';
 
 import { base_api } from 'services/api';
 
-// export const addSeller = (data) => {
-//   return dispatch => {
-//     dispatch({ type: 'ADD_SELLER' });
-//     Axios({
-//       method: 'post',
-//       url: base_api + 'addSeller',
-//       data
-//     })
-//     .then((res) => {
-//       console.log(res, 'res');
-//       // const data = res.data.data;
-//       // dispatch({ type: 'ADD_SELLER_SUCCESS', res });
-//     })
-//     .catch((error) => {
-//       console.log(error, 'error');
-//
-//       // dispatch({ type: 'ADD_SELLER_ERROR' });
-//     });
-//   }
-// }
-
 export const addSeller = (data) => {
   return dispatch => {
     dispatch({ type: 'ADD_SELLER' });
-    // POST request using fetch()
-    fetch('https://dev.dummy-api.alamisharia.co.id/addSeller', {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    Axios({
+      method: 'post',
+      url: base_api + 'addSeller',
+      data
     })
-
-    // Converting to JSON
-    .then(response => {
-      console.log(response, 'response');
-      response.json()
+    .then((res) => {
+      console.log(res, 'res');
+      // const data = res.data.data;
+      // dispatch({ type: 'ADD_SELLER_SUCCESS', res });
     })
-
-    // Displaying results to console
-    .then(json => console.log(json, 'json'))
     .catch((error) => {
-      console.error('Error:', error);
+      console.log(error, 'error');
+
+      // dispatch({ type: 'ADD_SELLER_ERROR' });
     });
   }
 }
+
+// export const addSeller = (data) => {
+//   return dispatch => {
+//     dispatch({ type: 'ADD_SELLER' });
+//     // POST request using fetch()
+//     fetch('https://dev.dummy-api.alamisharia.co.id/addSeller', {
+//         method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//         headers: {
+//         'Content-Type': 'application/json'
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       body: JSON.stringify(data) // body data type must match "Content-Type" header
+//     })
+//
+//     // Converting to JSON
+//     .then(response => {
+//       console.log(response, 'response');
+//       response.json()
+//     })
+//
+//     // Displaying results to console
+//     .then(json => console.log(json, 'json'))
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+//   }
+// }
